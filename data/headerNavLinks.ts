5ad1e8a6f9005a919c3d28e64f63cf150e917aab
云微 <1067852565@qq.com>
2025-03-06 13:44:39 -0800

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: '/', title: '主页' },
-  { href: '/search', title: '检索' },
   { href: '/projects', title: '网站导航' },
   { href: '/blog', title: '博客' },
   { href: '/about', title: '关于' },
